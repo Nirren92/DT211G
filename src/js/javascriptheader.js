0116853ecba2aa001101f06d5cd8048011a25de1
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-01-17 23:03:46 +0100

    ändrat i ifsats. borde kunna optimera denn mer sedan

--- src/js/javascriptheader.js
+++ src/js/javascriptheader.js
@@ -1,8 +1,8 @@
 function show_menu()
 {
-    var x = document.getElementById("menu_knappen").src;
+    
 
-    if(x.includes("img/cancel.png"))
+    if(document.getElementById("menu_knappen").src.includes("img/cancel.png"))
     {
         document.getElementById("menu_innehall").style.display="none";
         document.getElementById("menu_knappen").src="img/menu.png"

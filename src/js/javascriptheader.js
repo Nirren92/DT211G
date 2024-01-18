587a79674ad7b53f397487b8db3f92ce95b1fbcf
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-01-18 23:03:45 +0100

    ändringar

--- src/js/javascriptheader.js
+++ src/js/javascriptheader.js
@@ -1,9 +1,16 @@
+
+
+
+
+
 function show_menu()
 {
-    
+   
+
 
     if(document.getElementById("menu_knappen").src.includes("img/cancel.png"))
     {
+        
         document.getElementById("menu_innehall").style.display="none";
         document.getElementById("menu_knappen").src="img/menu.png"
        
@@ -17,6 +24,10 @@ function show_menu()
     }
   
     
-   
+    const dynamicImage = document.getElementById('menu_knappen');
+
+    // Sätt den dynamiska sökvägen till bilden
+    const imagePath = 'img/cancel.png';
+    dynamicImage.src = imagePath;
 
 }
